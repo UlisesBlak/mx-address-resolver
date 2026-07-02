da9359fde71158ab9a39507c31acea3a6053aa22
Ulises <Ulisesblak.dev@gmail.com>
2026-07-01 18:18:53 -0600

    chore: genereacion del esqueleto con spring boot 4.1.0

---
+++ src/main/java/io/github/ulisesblak/mxaddressresolver/MxAddressResolverApplication.java
@@ -0,0 +1,13 @@
+package io.github.ulisesblak.mxaddressresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxAddressResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxAddressResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ulisesblak/mxaddressresolver/MxAddressResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ulisesblak.mxaddressresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxAddressResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
